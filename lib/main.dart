f614655c025d5ddefcf4ff1a41a0126c9323acbe
Usman Afzal <56322052+UsmanAfzal8@users.noreply.github.com>
2022-11-28 23:03:17 +0500

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,8 @@ import 'screens/add_item/add_item_screen.dart';
 import 'screens/sale_screen/sale_screen.dart';
 import 'screens/screens.dart';
 
-const apiKey = 'AIzaSyBqcG1lPhbYwfTbcoOgFkOhbntIH81oTTo';
-const projectID = 'ramzan-hospital-qds';
+const apiKey = 'AIzaSyD8-XxjB82a_20VvqUR0ljtCjy9KL4MICU';
+const projectID = 'temporary-project-f558a';
 void main() {
   Firestore.initialize(projectID);
   FirebaseAuth.initialize(apiKey, VolatileStore());
