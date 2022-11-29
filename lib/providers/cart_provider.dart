26c1c0bce823cee47148644c4b7b6bc015a6e72b
Usman Afzal <56322052+UsmanAfzal8@users.noreply.github.com>
2022-11-29 19:44:42 +0500

    cart provider update

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/cupertino.dart';
 
-class CartProvider extends ChangeNotifier{
-  
+import '../models/item/cart_item.dart';
+import '../models/item/item.dart';
+
+class CartProvider extends ChangeNotifier {
+  final List<CartItem> _cartItems = <CartItem>[];
+  List<CartItem> get cartItem => _cartItems;
+
+  void addtocart(Item value, int quantity) async {
+    CartItem temp = CartItem(
+      discount: 0,
+      itemID: value.id,
+      price: value.salePrice * quantity,
+      salemanID: 'dev markaz',
+      quantity: quantity,
+    );
+    _cartItems.add(temp);
+    notifyListeners();
+  }
 }
