721102190fe8e284ab3ad7bca1b866c40f935b4c
Usman Afzal <56322052+UsmanAfzal8@users.noreply.github.com>
2022-12-15 23:49:34 +0500

    sale provider update

--- lib/widgets/sale/sale_bottom.dart
+++ lib/widgets/sale/sale_bottom.dart
@@ -6,11 +6,17 @@ import '../../providers/cart_provider.dart';
 import '../../providers/patient_provider.dart';
 import '../../providers/slip_provider.dart';
 
-class SaleBottom extends StatelessWidget {
+class SaleBottom extends StatefulWidget {
   const SaleBottom({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<SaleBottom> createState() => _SaleBottomState();
+}
+
+class _SaleBottomState extends State<SaleBottom> {
+  bool isloading = true;
   @override
   Widget build(BuildContext context) {
     return Consumer3<SlipProvider, PatientProvider, CartProvider>(builder:
@@ -25,34 +31,43 @@ class SaleBottom extends StatelessWidget {
       return cartPro.cartItem.isNotEmpty
           ? GestureDetector(
               onTap: () async {
+                setState(() {
+                  isloading = false;
+                });
                 await slipPro.addslip(patientID, totalBill, test,
                     customerDiscount, adjustment, amountPaid);
                 cartPro.emptyCart();
+                setState(() {
+                  isloading = true;
+                });
               },
-              child: Container(
-                height: 160,
-                width: double.infinity,
-                color: Colors.blue,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 150,
-                      padding: const EdgeInsets.symmetric(horizontal: 22),
-                      decoration: BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(12)),
-                      alignment: Alignment.center,
-                      child: const Text(
-                        'Print',
-                        style: TextStyle(color: Colors.white, fontSize: 32),
+              child: isloading
+                  ? Container(
+                      height: 160,
+                      width: double.infinity,
+                      color: Colors.blue,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 150,
+                            padding: const EdgeInsets.symmetric(horizontal: 22),
+                            decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(12)),
+                            alignment: Alignment.center,
+                            child: const Text(
+                              'Print',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 32),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
-                ),
-              ),
+                    )
+                  : const CircularProgressIndicator(),
             )
-          : SizedBox();
+          : const SizedBox();
     });
   }
 }
