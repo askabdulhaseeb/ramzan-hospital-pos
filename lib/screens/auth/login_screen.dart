afc9a32104e5eb3f0dfefc93be222b1c1e841737
askabdulhaseeb <askabdulhaseeb@gmail.com>
2023-02-23 07:49:37 -0800

    PRINT

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -149,7 +149,7 @@ class _SignInScreenState extends State<SignInScreen> {
                                     }
                                   },
                                   style: ButtonStyle(
-                                    iconColor:
+                                    foregroundColor:
                                         MaterialStateProperty.all(Colors.white),
                                     backgroundColor:
                                         MaterialStateProperty.all(Colors.blue),
